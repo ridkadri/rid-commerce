b9c024c44d8c52f0ccc5ee6e9a5b5a469e417569
Ridwan <kadriridwan0@gmail.com>
2020-05-08 17:09:10 +0000

    broken down homepage into components and added withRouterHOC for navigation

---
+++ src/pages/Homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './homepage.scss';
+
+import Directory from '../../components/Directory/directory';
+
+
+const Homepage = () => (
+  <div className='homepage'>
+    <Directory/>
+  </div>
+);
+  
+export default Homepage;
+
+
